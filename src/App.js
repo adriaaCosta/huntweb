e83993b95216dbb2530684e0f258c22d0e8c34de
adriaaCosta <adria.c@outlook.com>
2020-04-16 22:29:58 -0400

    routes, details

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import React from 'react';
-import Header from './components/Header';
+import React, { Component } from 'react';
 
+import Routes from './routes';
 import './styles.css';
-import Main from './pages/main';
+import Header from './components/Header';
+//import Main from './pages/main';
 
 const App = () => (
   <div className = "App"> 
     <Header/>
-    <Main />
+    <Routes />
   </div>
 );
 
